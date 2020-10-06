ee7f9e9fe1cdfcaad694c7268ad327fb95f8976e
KushnerMikalai <nickolaikushner@gmail.com>
2020-10-07 00:18:22 +0300

    feat: input / safe theme / title

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import { Helmet } from 'react-helmet'
 import './toggleThemes.css'
 import stylesTheme from './themes.module.css'
+import stylesApp from './App.module.css'
 
 function App() {
   const [inputBin, setInputBin] = useState('')
   const [inputDec, setInputDec] = useState('')
   const [isLightTheme, changeTheme] = useState(true)
 
+  useEffect(() => {
+    const data = sessionStorage.getItem('is_light_theme');
+    changeTheme(!!Number(data))
+  }, [])
+
   const toggleTheme = value => {
+    sessionStorage.setItem('is_light_theme', String(value ? 1 : 0));
     changeTheme(value)
   }
 
@@ -38,40 +45,50 @@ function App() {
     }
   }
 
+  const title = 'Bin2Dec'
+
   return (
-    <div className={`${isLightTheme ? stylesTheme.light : stylesTheme.dark} app`}>
-      <div className="change-theme">
-        <input
-            className="change-theme__input"
-            type="checkbox"
-            checked={isLightTheme}
-            onChange={e => toggleTheme(e.target.checked)}
-        />
-      </div>
-      <div className="container">
-        <div className="content">
-          <h1 className="title">Bin2Dec</h1>
-          <div>Binary</div>
-          <div className="input">
-            // TODO input style
-            <input
-                type="text"
-                name="binary"
-                placeholder="Enter 0 or 1"
-                value={inputBin}
-                onChange={e => changeBinary(e.target.value)}
-            />
-          </div>
-          <div>Decimal</div>
+    <React.Fragment>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <div className={`${isLightTheme ? stylesTheme.light : stylesTheme.dark} app`}>
+        <div className="change-theme">
           <input
-              type="number"
-              name="decimal"
-              value={inputDec}
-              onChange={e => changeDecimal(e.target.value)}
+              className="change-theme__input"
+              type="checkbox"
+              checked={isLightTheme}
+              onChange={e => toggleTheme(e.target.checked)}
           />
         </div>
+        <div className="container">
+          <div className="content">
+            <h1 className="title">{title}</h1>
+            <div className={stylesApp.input}>
+              <label className={stylesApp.inputLabel}>Binary</label>
+              <input
+                  className={stylesApp.inputInner}
+                  type="text"
+                  name="binary"
+                  placeholder="Enter 0 or 1"
+                  value={inputBin}
+                  onChange={e => changeBinary(e.target.value)}
+              />
+            </div>
+            <div className={stylesApp.input}>
+              <label className={stylesApp.inputLabel}>Decimal</label>
+              <input
+                  className={stylesApp.inputInner}
+                  type="number"
+                  name="decimal"
+                  value={inputDec}
+                  onChange={e => changeDecimal(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 
